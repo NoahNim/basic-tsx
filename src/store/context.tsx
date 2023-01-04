28b4383a04d4505f24f027442a41eda261ad0db1
Noah Medoff <nmedoff123@gmail.com>
2023-01-04 15:10:52 -0800

    build context with retrieving api data function

--- src/store/context.tsx
+++ src/store/context.tsx
@@ -1,17 +1,40 @@
-import { createContext } from "react";
+import { produceWithPatches } from "immer";
+import { createContext, ReactNode, useState } from "react";
 
 interface ActivityContextInterface {
     activityData: Object
-    activity: string
-    participants: number
-    price: number
-    props: any
+    // activity: string
+    // participants: number
+    // price: number
 }
 
 const ActivityCtx = createContext<ActivityContextInterface | null>(null);
 
-const ActivityContextProvider = ({ activityData, activity, participants, price, props }: ActivityContextInterface) => {
+const ActivityContextProvider = ({ children }: { children: ReactNode, }, { activityData }: ActivityContextInterface) => {
+    const [currActivityData, setCurrActivityData] = useState<Object>({});
 
+    const getActivity = async () => {
+        try {
+            const res = await fetch(`https://www.boredapi.com/api/activity`)
+
+            if (res.ok) {
+                let data: Object = res.json();
+                setCurrActivityData(data);
+                return data
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <ActivityCtx.Provider
+            value={{
+                activityData: currActivityData,
+            }}>
+            {children}
+        </ActivityCtx.Provider>
+    )
 }
 
 export default ActivityCtx
